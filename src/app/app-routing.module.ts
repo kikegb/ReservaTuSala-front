228ce7ca9488ffe47cae2fd4750a784c3ed0239e
Enrique Gómez <egomez@profile.es>
2023-02-08 09:44:33 +0100

    admin home page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ const routes: Routes = [
   
   { path: 'admin/home', loadChildren: () => import('./pages/admin/home/home.module').then(m => m.HomeModule) },
   
+  { path: 'admin/users', loadChildren: () => import('./pages/admin/users/users.module').then(m => m.UsersModule) },
+  
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
