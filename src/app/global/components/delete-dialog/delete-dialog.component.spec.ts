260bd07d63b1a2c21b463e630a95b6a143d02505
Enrique Gómez <egomez@profile.es>
2023-02-15 09:33:49 +0100

    delete-dialog tests done

--- src/app/global/components/delete-dialog/delete-dialog.component.spec.ts
+++ src/app/global/components/delete-dialog/delete-dialog.component.spec.ts
@@ -1,23 +1,42 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRefMock } from 'src/test-helpers/mocks/mat-dialog-ref-mock';
 import { DeleteDialogComponent } from './delete-dialog.component';
 
 describe('DeleteDialogComponent', () => {
   let component: DeleteDialogComponent;
   let fixture: ComponentFixture<DeleteDialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DeleteDialogComponent ]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DeleteDialogComponent ],
+      providers: [
+        {provide: MatDialogRef, useValue: MatDialogRefMock},
+        {provide: MAT_DIALOG_DATA, useValue: []}
+      ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(DeleteDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should close and return false when calling onCancel method', () => {
+    spyOn(component.dialogRef, 'close');
+    component.onCancel();
+    expect(component.dialogRef.close).toHaveBeenCalled();
+    expect(component.dialogRef.close).toHaveBeenCalledWith(false);
+  });
+
+  it('should close and return true when calling onConfirm method', () => {
+    spyOn(component.dialogRef, 'close');
+    component.onConfirm();
+    expect(component.dialogRef.close).toHaveBeenCalled();
+    expect(component.dialogRef.close).toHaveBeenCalledWith(true);
   });
 });
---
+++ src/test-helpers/mocks/mat-dialog-ref-mock.ts
@@ -0,0 +1,9 @@
+export const MatDialogRefMock = {
+    open: (value: any) => {
+        return value;
+    },
+
+    close: (value: any) => {
+        return value;
+    }
+};
