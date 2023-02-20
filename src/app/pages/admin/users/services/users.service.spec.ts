eb9267165c466f9273535507619b61f71bcb6ed5
Enrique Gómez <egomez@profile.es>
2023-02-20 11:15:17 +0100

    users service tested

--- src/app/pages/admin/users/services/users.service.spec.ts
+++ src/app/pages/admin/users/services/users.service.spec.ts
@@ -1,16 +1,137 @@
-import { TestBed } from '@angular/core/testing';
-
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UsersService } from './users.service';
+import { UsersServiceMock } from 'src/test-helpers/mocks/users-service-mock';
+import { User } from '../interfaces/user.interface';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let backend: HttpTestingController;
+
+  const usersList = <User[]>[
+    {
+        "deleted": false,
+        "id": 1,
+        "cnif": "87654321X",
+        "name": "Business S.A.",
+        "phone": "999999999",
+        "password": "psswd",
+        "email": "email@example.com",
+        "role": "BUSINESS",
+    },
+    {
+        "deleted": true,
+        "id": 2,
+        "cnif": "12345678Y",
+        "name": "Custo Mer",
+        "phone": "999999999",
+        "password": "p4ssw0rd",
+        "email": "mail@mail.com",
+        "role": "CUSTOMER",
+    }
+  ];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        UsersService
+      ]
+    });
     service = TestBed.inject(UsersService);
+    backend = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should GET list of users with getUsers()', fakeAsync(() => {
+    let response: User[] = [];
+
+    service.getUsers().subscribe(
+      (receivedResponse: User[]) => {
+        response = receivedResponse;
+      },
+      (error: any) => {}
+    );
+
+    const requestWrapper = backend.expectOne({url: 'http://localhost:8081/user/all'});
+    requestWrapper.flush(usersList);
+    backend.verify();
+
+    tick();
+
+    expect(requestWrapper.request.method).toEqual('GET');
+    expect(response).toEqual(usersList);
+  }));
+
+  it('should POST user with addUser()', fakeAsync(() => {
+    let response: User = <User>{};
+    const user = usersList[0];
+
+    service.addUser(user).subscribe(
+      (receivedResponse: User) => {
+        response = receivedResponse;
+      },
+      (error: any) => {}
+    );
+
+    const requestWrapper = backend.expectOne({url: 'http://localhost:8081/user'});
+    requestWrapper.flush(user);
+    backend.verify();
+
+    tick();
+
+    expect(requestWrapper.request.method).toEqual('POST');
+    expect(response).toEqual(user);
+  }));
+
+  it('should PUT user with updateUser()', fakeAsync(() => {
+    let response: User = <User>{};
+    const user = usersList[0];
+
+    service.updateUser(user).subscribe(
+      (receivedResponse: User) => {
+        response = receivedResponse;
+      },
+      (error: any) => {}
+    );
+
+    const requestWrapper = backend.expectOne({url: 'http://localhost:8081/user'});
+    requestWrapper.flush(user);
+    backend.verify();
+
+    tick();
+
+    expect(requestWrapper.request.method).toEqual('PUT');
+    expect(response).toEqual(user);
+  }));
+
+  it('should delete user with addUser()', fakeAsync(() => {
+    let response = {};
+    const expectedResponse = {
+      "code": 0,
+      "description": "OK"
+    };
+    const user = usersList[0];
+
+    service.deleteUser(user.id).subscribe(
+      (receivedResponse: any) => {
+        response = receivedResponse;
+      },
+      (error: any) => {}
+    );
+
+    const requestWrapper = backend.expectOne({url: 'http://localhost:8081/user?id=' + user.id});
+    requestWrapper.flush(expectedResponse);
+    backend.verify();
+
+    tick();
+
+    expect(requestWrapper.request.method).toEqual('DELETE');
+    expect(response).toEqual(expectedResponse);
+  }));
 });
