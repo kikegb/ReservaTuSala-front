557f7234bb387f6f11f158fadbfc799a67799b70
Enrique Gómez <egomez@profile.es>
2023-02-20 09:28:42 +0100

    mock services for testing

---
+++ src/test-helpers/mocks/sidenav-service-mock.ts
@@ -0,0 +1,8 @@
+import { MatSidenav } from "@angular/material/sidenav";
+
+export const SidenavServiceMock = {
+    setSidenav: (sidenav: MatSidenav) => {},
+    open: () => {},
+    close: () => {},
+    toggle: () => {}
+}
---
+++ src/test-helpers/mocks/users-service-mock.ts
@@ -0,0 +1,44 @@
+import { of } from "rxjs";
+import { User } from "src/app/pages/admin/users/interfaces/user.interface";
+
+const usersList = <User[]>[
+  {
+      "deleted": false,
+      "id": 1,
+      "cnif": "87654321X",
+      "name": "Business S.A.",
+      "phone": "999999999",
+      "password": "psswd",
+      "email": "email@example.com",
+      "role": "BUSINESS",
+  },
+  {
+      "deleted": true,
+      "id": 2,
+      "cnif": "12345678Y",
+      "name": "Custo Mer",
+      "phone": "999999999",
+      "password": "p4ssw0rd",
+      "email": "mail@mail.com",
+      "role": "CUSTOMER",
+  }
+];
+
+export const UsersServiceMock = {
+
+    getUsers: () => {
+      return of(usersList);
+    },
+
+    addUser: (user: User) => {
+        return of(user as User);
+    },
+
+    updateUser: (user: User) => {
+      return of(user as User);
+    },
+
+    deleteUser: (id: number) => {
+      return of([]);
+    }
+  }
