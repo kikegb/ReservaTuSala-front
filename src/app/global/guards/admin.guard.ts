2574f9b76af1cfe17444c200d0699c9401143e9b
Enrique Gómez <egomez@profile.es>
2023-03-01 08:43:37 +0100

    admin guard done

---
+++ src/app/global/guards/admin.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import jwtDecode from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = <any>jwtDecode(token);
+      const role = decodedToken.role;
+      if (role == "ADMIN") {
+        return true;
+      }
+      this.router.navigate(['/unauthorized']);
+      return false;
+    }
+    this.router.navigate(['/login']);
+    return false;
+  }
+  
+}
